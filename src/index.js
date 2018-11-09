04726f8f5e65642cc5deda4ad0351d5d4ce26091
Gleb VorontСЃov <g.vorontcov@corp.vk.com>
2018-11-09 16:33:26 +0300

    SW and gh-pages

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import connect from '@vkontakte/vkui-connect';
 import App from './App';
+import registerServiceWorker from './sw';
 import './main.css';
-
 
 // Init VK App
 connect.send('VKWebAppInit', {});
+
+// Service Worker For Cache
+registerServiceWorker();
 
 ReactDOM.render(<App />, document.getElementById('root'));
