5315d24a40950487e6b1e9663ca9899d806df64f
Gleb VorontСЃov <g.vorontcov@corp.vk.com>
2018-11-09 12:08:13 +0300

    VK Connect, React and styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+import 'core-js/es6/map';
+import 'core-js/es6/set';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import connect from '@vkontakte/vkui-connect';
+import './main.css';
+
+
+// Init VK App
+connect.send('VKWebAppInit', {});
+
+ReactDOM.render(<h1 style={{textAlign: 'center'}}>Hello world!</h1>, document.getElementById('root'));
